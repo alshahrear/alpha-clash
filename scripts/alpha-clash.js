17878298dc280635089c6b7ef95b3fccda8481b1
alshahrear <alshahrear777@gmail.com>
2024-12-10 00:03:23 +0800

    added a half

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,23 @@
+// function play(){
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+
+//     const playGroundScreen = document.getElementById('play-ground');
+//     playGroundScreen.classList.remove('hidden');
+// }
+
+function continueGame(){
+    const alphabet = getRandomAlphabet();
+    console.log(alphabet);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
-    console.log('play now');
+    hideElementById('home-screen')
+    showElementById('play-ground')
+    continueGame()
 }
